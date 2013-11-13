6fe3242168ee5c4ddae4c1a9d81fcd649d172520
Cody Smith <codysmithd@gmail.com>
2013-11-13 11:06:31 -0500

    Working page-specific style changes

--- _includes/navbar/navbar.js
+++ _includes/navbar/navbar.js
@@ -1,52 +1,21 @@
 var navbar_parent = null;
+var navbar_height = 0;
 
 readyFunctions.push(function(){
 	navbar_parent = $("#navbar_container");
+	navbar_height = navbar_parent.height();
 });
 
 // Changes the scrolling behavior depending on the page
 parallaxFunctions.push(function(){
-	switch(pages[current_full_page]){
-		case "home":
-			//scrolling_home();
-			break;
-		case "the_grid":
-			//scrolling_the_grid();
-			break;
-		case "projects":
-			//scrolling_the_grid();
-			break;
-		case "about":
-			//scrolling_the_grid();
-			break;
-		default:
+	// Going up
+	if( (current_full_page*div_height) - parent.scrollTop() > navbar_height){
+		navbar_parent.attr("class", "navbar_container-" + pages[current_full_page-1]);
+	}
+	else{
+		navbar_parent.attr("class", "navbar_container-" + pages[current_full_page]);
 	}
 });
-
-// Used when scrolling into or out of 'Home'
-function scrolling_home(){
-	
-}
-
-function scolling_the_grid(){
-	// If scrolling up [into home]
-	if(current_full_page*div_height > parent.scrollTop()){
-		
-	}
-	// If scrolling down into projects
-}
-
-function scolling_projects(){
-	// Decide what direction we are scrolling
-	// If scrolling up into home
-	// If scrolling down into projects
-}
-
-function scolling_about(){
-	// Decide what direction we are scrolling
-	// If scrolling up into home
-	// If scrolling down into projects
-}
 
 
 // Changes the active link when the page changes
@@ -57,5 +26,5 @@ changePageFunctions.push(function(){
 		else
 			document.getElementById("link-" + pages[i]).className = '';
 	}
-	navbar_parent.attr("class", "navbar_container-" + pages[current_full_page]);
+	//navbar_parent.attr("class", "navbar_container-" + pages[current_page]);
 });
