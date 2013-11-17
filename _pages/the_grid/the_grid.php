1eb5429468da1998e8ebaea093b59f95e385f89a
Cody Smith <codysmithd@gmail.com>
2013-11-16 20:42:27 -0500

    Created PHP support

---
+++ _pages/the_grid/the_grid.php
@@ -0,0 +1,7 @@
+<div id= "frame_the_grid" class="frame">
+	<div style="position:relative; width: 100%; height:100%;">
+		<?php ?>
+		<div id="the_grid-background"> </div>
+		<div id="the_grid-background_2"> </div>
+	</div>
+</div>
