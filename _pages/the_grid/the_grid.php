0de135458ce79582e38ee4392acdfdb872f680a5
Cody Smith <codysmithd@gmail.com>
2013-11-18 12:36:38 -0500

    Created the GridView

--- _pages/the_grid/the_grid.php
+++ _pages/the_grid/the_grid.php
@@ -1,6 +1,6 @@
 <div id= "frame_the_grid" class="frame">
 	<div style="position:relative; width: 100%; height:100%;">
-		<?php ?>
+		<?php include "/var/www/_pages/the_grid/gridView.php;" ?>
 		<div id="the_grid-background"> </div>
 		<div id="the_grid-background_2"> </div>
 	</div>
