800c009481e511748d5b9e6c6d56093a95a5dd40
Cody Smith <codysmithd@gmail.com>
2013-11-19 23:28:20 -0500

    Added PHP logic for posts

--- _pages/the_grid/the_grid.php
+++ _pages/the_grid/the_grid.php
@@ -1,6 +1,15 @@
 <div id= "frame_the_grid" class="frame">
 	<div style="position:relative; width: 100%; height:100%;">
-		<?php include "/var/www/_pages/the_grid/gridView.php;" ?>
+		<?php
+			include "/var/www/_includes/_markdownParser/markdownParser.php";
+			foreach(scandir("/var/www/_pages/the_grid/posts") as $x){
+				if($x != "." and $x != ".."){
+					echo "<div class=\"the_grid-Post\">";
+					parse_and_output_file($x);
+					echo "</div>";
+				}
+			}
+		?>
 		<div id="the_grid-background"> </div>
 		<div id="the_grid-background_2"> </div>
 	</div>
