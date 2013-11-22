e790b5cc812c42ee28561aaf3eb4d41ecfc8dcf3
Cody Smith <codysmithd@gmail.com>
2013-11-22 10:03:50 -0500

    Created layout

--- _pages/the_grid/the_grid.php
+++ _pages/the_grid/the_grid.php
@@ -1,15 +1,23 @@
 <div id= "frame_the_grid" class="frame">
 	<div style="position:relative; width: 100%; height:100%;">
-		<?php
-			include "/var/www/_includes/_markdownParser/markdownParser.php";
-			foreach(scandir("/var/www/_pages/the_grid/posts") as $x){
-				if($x != "." and $x != ".."){
-					echo "<div class=\"the_grid-Post\">";
-					parse_and_output_file($x);
-					echo "</div>";
-				}
-			}
-		?>
+			<div id="the_grid-posts_wrapper">
+				<div id="the_grid-posts">
+					<?php
+						include "/var/www/_includes/_markdownParser/markdownParser.php";
+						foreach(scandir("/var/www/_pages/the_grid/posts") as $x){
+							if($x != "." and $x != ".."){
+								echo "<div class=\"the_grid-post\">";
+								parse_and_output_file("/var/www/_pages/the_grid/posts/$x");
+								echo "</div>";
+							}
+						}
+					?>
+				</div>
+			</div>
+		<div id="the_grid-navigation_wrapper">
+			<div class="the_grid-navigation_link the_grid-navigation_link-current"></div>
+			<div class="the_grid-navigation_link"></div>
+		</div>
 		<div id="the_grid-background"> </div>
 		<div id="the_grid-background_2"> </div>
 	</div>
