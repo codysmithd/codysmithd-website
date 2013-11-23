65ee9e97058aee689e34d5bee71747ccd0288392
Cody Smith <codysmithd@gmail.com>
2013-11-23 16:26:55 -0500

    Added <p> functionality

--- _includes/_markdownParser/markdownParser.php
+++ _includes/_markdownParser/markdownParser.php
@@ -1,10 +1,19 @@
 <?php
+// Line by line conversion from Markdown to HTML
+// NOTE: Only supports h1-h3, p, and text formatting (not lists or anything else like that)
 function parse_and_output_file($x){
-	$handle = @fopen($x, "r");
-	if($handle){
-		while (($buffer = fgets($handle)) !== false) {
-			echo $buffer;
+	$final_output = "";
+	$text = file_get_contents ($x);
+	if($text){
+			
+		#Create new <p> on empty spaces 
+		$pTags = explode("\n\n", $text);
+		foreach ($pTags as $x) {
+			$final_output .= "<p>$x</p>";
 		}
+		
+		#output
+		echo $final_output;
 	}
 }
 ?>
