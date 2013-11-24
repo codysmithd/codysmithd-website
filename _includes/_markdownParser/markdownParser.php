d3faf0fb008d87e8603ee2d0eca97b7f20630d9f
Cody Smith <codysmithd@gmail.com>
2013-11-23 23:48:09 -0500

    h1, h2, h3, bold, italic, and code functionality added

--- _includes/_markdownParser/markdownParser.php
+++ _includes/_markdownParser/markdownParser.php
@@ -1,19 +1,59 @@
 <?php
-// Line by line conversion from Markdown to HTML
-// NOTE: Only supports h1-h3, p, and text formatting (not lists or anything else like that)
+
+// Convert markdown file to HTML and output it
 function parse_and_output_file($x){
 	$final_output = "";
-	$text = file_get_contents ($x);
+	$text = file_get_contents($x);
 	if($text){
-			
-		#Create new <p> on empty spaces 
-		$pTags = explode("\n\n", $text);
-		foreach ($pTags as $x) {
-			$final_output .= "<p>$x</p>";
+		
+		# Explode by sets of two new-lines
+		foreach (explode("\n\n", $text) as $x) {
+			if(substr($x, -3, 3) == "==="){
+				$buffer = substr($x, 0, strrpos($x, "\n"));
+				$buffer = parse_single_line($buffer);
+				$final_output .= "<h1>$buffer</h1>";
+			}
+			else if(substr($x, -3, 3) == "---"){
+				$buffer = substr($x, 0, strrpos($x, "\n"));
+				$buffer = parse_single_line($buffer);
+				$final_output .= "<h2>$buffer</h2>";
+			}
+			else if(substr($x, 0, 4) == "### "){
+				$buffer = parse_single_line(substr($x, 4));
+				$final_output .= "<h3>$buffer</h3>";
+			}
+			else{
+				$buffer = parse_single_line($x);
+				$final_output .= "<p>$buffer</p>";
+			}
 		}
 		
 		#output
 		echo $final_output;
 	}
 }
+
+// PRIVATE: Parses single-line markdown commands (character by character)
+function parse_single_line($text){
+	$output = "";
+	$delim = array("italic" => false, "bold" => false, "code" => false);
+	foreach(preg_split("#(\*{1,2}|\`)#", $text,0, PREG_SPLIT_DELIM_CAPTURE | PREG_SPLIT_NO_EMPTY) as $x){
+		if($x === "*"){
+			$output .= ($delim["italic"] == true) ? '</i>' : '<i>';
+			$delim["italic"] = !$delim["italic"];
+		}
+		else if($x === "**"){
+			$output .= ($delim["bold"] == true) ? '</strong>' : '<strong>';
+			$delim["bold"] = !$delim["bold"];
+		}
+		else if($x === "`"){
+			$output .= ($delim["code"] == true) ? '</code>' : '<code>';
+			$delim["code"] = !$delim["code"];
+		}
+		else
+			$output .= $x;
+	}
+	return $output;
+}
+
 ?>
