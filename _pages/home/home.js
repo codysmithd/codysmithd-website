42eb7c1344c1bdaf587b2ee9fd9f679500c170c9
Cody Smith <codysmithd@gmail.com>
2013-12-06 13:10:55 -0500

    Optimized parallax function, added special URL update

--- _pages/home/home.js
+++ _pages/home/home.js
@@ -5,9 +5,16 @@ var home_location_in_pages = 0;
 
 // Called when the document is created
 readyFunctions.push(function(){
+	
 	home_background_layer = $("#home_background");
 	home_background_layer_mask = $("#home_background_mask");
+	
 	home_location_in_pages = pages.indexOf("home");
+	
+	URLrules[home_location_in_pages] = function(){
+		window.history.pushState("", "codysmithd", "/");
+	};
+	
 });
 
 // Called every time the page scrolls
@@ -16,7 +23,7 @@ parallaxFunctions.push(function(){
 	// Only do parallax effect when the page is in view
 	if(Math.abs(current_page - home_location_in_pages) <= 1){
 		
-		home_scrolled = parent.scrollTop() - div_height*pages.indexOf("home");
+		home_scrolled = heightFromTop - div_height*pages.indexOf("home");
 		home_background_layer.css("margin-top", home_scrolled*.5);
 		home_background_layer_mask.css("margin-top", home_scrolled*.3);
 	}
