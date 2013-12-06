335500d86caf2dc1c674ce1ef468650f1a2b3b2a
Cody Smith <codysmithd@gmail.com>
2013-12-06 13:09:43 -0500

    Got rid of extra layer

--- _pages/_404/404.js
+++ _pages/_404/404.js
@@ -1,6 +1,5 @@
 var _404_text_background = null;
 var _404_background_1 = null;
-//var _404_background_2 = null;
 
 var xAdjust = 0;
 var yAdjust = 0;
@@ -9,7 +8,6 @@ $(document).ready(function() {
 	
 	_404_text_background = $("#_404_text");
 	_404_background_1 = $("#_404_background-layer1");
-	//_404_background_2 = $("#_404_background-layer2");
 	
 	yAdjust = $(window).outerHeight();
 	xAdjust = $(window).outerWidth();
@@ -17,9 +15,7 @@ $(document).ready(function() {
 	
 	$("#_404_frame").mousemove(function(e){
     	_404_text_background.css("background-position", e.clientX*0.01 + "px " + e.clientY*0.01 + "px");
-    	_404_background_1.css("top", (e.clientY-yAdjust)*0.1 + "px");
-    	_404_background_1.css("left", (e.clientX-xAdjust)*0.1 + "px");
-    	//_404_background_2.css("top", (e.clientY-yAdjust)*0.2 + "px");
-    	//_404_background_2.css("left", (e.clientX-xAdjust)*0.2 + "px");
+    	_404_background_1.css("top", (e.clientY-yAdjust)*0.02 + "px");
+    	_404_background_1.css("left", (e.clientX-xAdjust)*0.02 + "px");
     });
 });
